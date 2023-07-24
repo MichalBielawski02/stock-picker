227d3a8c4808d56c7f676938eb9746edc249cbbe
Michal Bielawski <michal.bielawski02@gmail.com>
2023-07-24 19:08:13 +0200

    refactoring

---
+++ README.md
@@ -0,0 +1,3 @@
+A method that takes in an array of stock prices, one for each hypothetical day. It returns a pair of days representing the best day to buy and the best day to sell.
+
+This is the excercise from https://theodinproject.com.
--- stock-picker.rb
+++ stock-picker.rb
@@ -8,17 +8,15 @@ def stock_picker(stock_array)
     keys = []
     stock_array.each_with_index do |amount_sell, index_sell|
         stock_array.each_with_index do |amount_buy, index_buy|
-            if index_buy < index_sell && index_buy != index_sell && index_buy != 0 && index_sell != 0
+            if index_buy < index_sell && index_buy != index_sell && index_buy.between?(1, 5) && index_sell.between?(1, 5)
                 divide = amount_sell - amount_buy
                 result[divide] = [index_buy, index_sell] unless divide == 0
                 keys.push(divide)
             end
         end
     end
-    buy = result[keys.max].first
-    sell = result[keys.max].last
-    buy_day = weekday(buy)
-    sell_day = weekday(sell)
+    buy_day = weekday(result[keys.max].first)
+    sell_day = weekday(result[keys.max].last)
     puts "Best to buy on #{buy_day} and to sell on #{sell_day}."
 end
 
